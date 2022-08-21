e4bfb2c4ff541cf0e1a6274503b20044796f9d50
Nekomaster12 <artemmozorow@gmail.com>
2022-08-21 06:14:29 +0300

    ReadyToDo

---
+++ объекти/todolist/todo.js
@@ -0,0 +1,43 @@
+const ToDoList = {
+    createNewTask(taskName, taskStatus = "ToDo"){
+        taskName = taskName, taskStatus = taskStatus;
+        ToDoList[taskName] = taskStatus
+    },
+    changeStatus(taskName, newStatus){
+        if(taskName in ToDoList){
+            ToDoList[taskName] = newStatus
+            }else return("Такого таска нет.")
+    },
+    deleteTask(taskName){
+        if(taskName in ToDoList){
+            delete ToDoList[taskName]
+        }else return("Такого таска нет")
+    },
+    showList(){console.log("ToDo:")
+    for(let i in ToDoList)
+    {
+        if(typeof(ToDoList[i]) == "string" && ToDoList[i] == "ToDo"){console.log("     ",i)}
+    }
+    console.log("InProgress:")
+    for(let i in ToDoList)
+    {
+        if(typeof(ToDoList[i]) == "string" && ToDoList[i] == "InProgress"){console.log("     ",i)}
+    }
+    console.log("Done:")
+    for(let i in ToDoList)
+    {
+        if(typeof(ToDoList[i]) == "string" && ToDoList[i] == "Done"){console.log("     ",i)}
+    }
+    console.log("Other:")
+    for(let i in ToDoList)
+    {
+        if(typeof(ToDoList[i]) == "string" && ToDoList[i] != "Done" && ToDoList[i] != "InProgress" && ToDoList[i != "ToDo"]){console.log("     ",i)}
+    }
+    }
+}
+
+ToDoList.createNewTask("Посмотреть аниму")
+ToDoList.createNewTask("Пойти спать")
+ToDoList.createNewTask("Покушать", "Done")
+ToDoList.createNewTask("Доделать страду", "InProgress")
+ToDoList.showList("")
